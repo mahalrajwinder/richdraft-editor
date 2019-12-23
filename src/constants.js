09e494c5d0185099db0b2e3167dfefd5b86d3b9d
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-12-22 20:57:42 -0800

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2019 Rajwinder Singh
+// Licensed under the MIT license
+//
+// -----------------------------------------------------------------------------
+// File: constants.js
+// Author: Rajwinder Singh
+// -----------------------------------------------------------------------------
+
+export const ClassName = {
+  EDITOR: 'RichDraftEditor-editor',
+  HIDE_PLACEHOLDER: 'RichDraftEditor-editor-hidePlaceholder',
+  ROOT: 'RichDraftEditor-root'
+};
+
+
+export const Entity = {
+  LINK: 'LINK'
+};
+
+
+export const HANDLED = 'handled';
+export const NOT_HANDLED = 'not_handled';
+export const PLACEHOLDER = 'Tell a story';
