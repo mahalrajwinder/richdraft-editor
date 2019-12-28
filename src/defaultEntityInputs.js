16dc9256d0e5f6e98fd2b8c89bba014f4c3a53d7
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-12-27 19:13:51 -0800

    Create defaultEntityInputs.js

---
+++ src/defaultEntityInputs.js
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2019 Rajwinder Singh
+// Licensed under the MIT license
+//
+// -----------------------------------------------------------------------------
+// File: defaultActions.js
+// Author: Rajwinder Singh
+// -----------------------------------------------------------------------------
+//
+// This file contains an object containing default input components for the
+// default entities.
+
+import LinkInput from './components/LinkInput';
+
+
+export default {
+  LINK: LinkInput
+};
