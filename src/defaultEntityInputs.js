fcec711758daa3f935992da86905041766c58ba9
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-12-27 19:38:51 -0800

    Fix filename comment

--- src/defaultEntityInputs.js
+++ src/defaultEntityInputs.js
@@ -3,7 +3,7 @@
 // Licensed under the MIT license
 //
 // -----------------------------------------------------------------------------
-// File: defaultActions.js
+// File: defaultEntityInputs.js
 // Author: Rajwinder Singh
 // -----------------------------------------------------------------------------
 //
